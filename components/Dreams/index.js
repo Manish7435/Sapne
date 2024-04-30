73c74f612dfe990ea77686e6854c78234ec7054c
man-ishh <manishd.kumar@cogoport.com>
2024-04-30 10:56:48 +0530

    build fixed

--- components/Dreams/index.js
+++ components/Dreams/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Container from "../Container"
 import SingleDream from "./SingleDream"
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from '@/store/CounterSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { increment, decrement } from '@/store/CounterSlice';
 
 const Dreams = ()=>{
-  const dispatch = useDispatch();
-  const count = useSelector(state => state.counter.value);
+  // const dispatch = useDispatch();
+  // const count = useSelector(state => state.counter.value);
   return(
     <div className=" sm:py-20">
-       <h1>Counter: {count}</h1>
-       <button onClick={() => dispatch(increment())}>Increment</button>
+       {/* <h1>Counter: {count}</h1>
+       <button onClick={() => dispatch(increment())}>Increment</button> */}
     <Container >
       <SingleDream endPoint={'/api/dream/get'}/>
     </Container>
